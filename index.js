0987f15efe911efeb67b667f8c308a259a08a8a9
Stuart P. Bentley <stuart@testtrack4.com>
2016-02-22 03:38:03 +0000

    Add modal ops

--- index.js
+++ index.js
@@ -5,10 +5,26 @@ var teListingEntry = cre('.listing', {wall: true}, [
     cre('div', {part: 'config-editor'}),
   ]),
   cre('.section', {part: 'op-section'}, [
-    cre('button', {part: 'dir-chooser', type: 'button'}, 'Choose directory'),
-    cre('button', {part: 'config-save', type: 'button'}, 'Save config'),
-    cre('button', {part: 'pull-button'}, 'Pull'),
-    cre('output', {part: 'pull-status'})
+    cre('.section', {part: 'no-dir', hidden: true}, [
+      cre('button', {part: 'dir-chooser', type: 'button'}, 'Choose directory')
+    ]),
+    cre('.section', {part: 'modified-config', hidden: true}, [
+      cre('button', {part: 'config-save', type: 'button'},
+        'Save config'),
+      cre('button', {part: 'config-revert', type: 'button'},
+        'Discard changes'),
+    ]),
+    cre('.section', {part: 'pull-ready', hidden: true}, [
+      cre('.tightline', {part: 'dir-line'}, [
+        cre('span', {part: 'dir-location'}),
+        cre('button', {part: 'dir-changer', type: 'button'}, 'Change'),
+      ]),
+      cre('button', {part: 'pull-button'}, 'Pull')
+    ]),
+    cre('.section', {part: 'pull-ongoing', hidden: true}, [
+      cre('output', {part: 'pull-status'})
+      // TODO: cancel button
+    ])
   ])
 ]);
 
@@ -19,19 +35,73 @@ function createListingObject() {
 function createListingEntry(listing) {
   var listingEntry = cre(teListingEntry);
 
+  var opSection = listingEntry.getPart('op-section');
+  function showOps(element) {
+    for (var i = 0; i < opSection.children.length; i++) {
+      var mode = opSection.children[i];
+      mode.hidden = mode != element;
+    }
+  }
+  var noDirOps = listingEntry.getPart('no-dir');
+  var modifiedConfigOps = listingEntry.getPart('modified-config');
+  var pullReadyOps = listingEntry.getPart('pull-ready');
+  var ongoingOps = listingEntry.getPart('pull-ongoing');
+
+  // TODO: determine if operation is in progress externally,
+  // everywhere this is currently used
+  var pullInProgress = false;
+
+  function showApplicableOps() {
+    if (editor.getValue == listing.config) {
+      if (pullInProgress) {
+        return showOps(ongoingOps);
+      } else {
+        return showOps(pullReadyOps);
+      }
+    } else {
+      showOps(modifiedConfigOps);
+    }
+  }
+
+  var dirLocationSpan = listingEntry.getPart('dir-location');
+
+  function displayPath(path) {
+    dirLocationSpan.textContent = path;
+  }
+
+  if (listing.retainedDirId) {
+    dirEntries.getDisplayPath(listing.retainedDirId).then(function (path) {
+      if (path) {
+        displayPath(path);
+        return showApplicableOps();
+      } else {
+        // TODO: revoke when the path is null (not restorable)
+        return showOps(noDirOps);
+      }
+    });
+  } else showOps(noDirOps);
+
   function updateConfig(configJson) {
+    // update our local copy for config-has-been-modified checking
+    listing.config = configJson;
+
     return listings.updateById(listing.id, 'config', configJson);
   }
 
-  listingEntry.getPart('dir-chooser')
-    .addEventListener('click', function (evt) {
-      dirEntries.chooseDir().then(function(dir){
-        return listings.updateById(
-          listing.id, 'retainedDirId', dir.retainedId);
-        // TODO: read directory for config
-        // TODO: check for manifest, auto-associate with extension
-      });
+  function selectDir () {
+    dirEntries.chooseDir().then(function(dir){
+      dirEntries.getEntryDisplayPath(dir).then(displayPath);
+      return listings.updateById(
+        listing.id, 'retainedDirId', dir.retainedId)
+        .then(showApplicableOps);
+      // TODO: read directory for config, if config is blank
     });
+  }
+
+  listingEntry.getPart('dir-chooser')
+    .addEventListener('click', selectDir);
+  listingEntry.getPart('dir-changer')
+    .addEventListener('click', selectDir);
 
   var editorElement = listingEntry.getPart('config-editor');
   var editor = ace.edit(editorElement);
@@ -43,10 +113,13 @@ function createListingEntry(listing) {
   editor.renderer.setOption('showLineNumbers', false);
   editor.setValue(listing.config || '', 1);
 
+  // update the visible operations every time the config is modified
+  session.on('change', showApplicableOps);
+
   function saveConfig() {
     var configJson = editor.getValue();
-    updateConfig(configJson);
-    // TODO: Acknowledge config update saved
+    return updateConfig(configJson)
+      .then(showApplicableOps);
   }
 
   listingEntry.getPart('config-save')
@@ -58,22 +131,34 @@ function createListingEntry(listing) {
     exec: saveConfig
   });
 
+  function revertConfig() {
+    // TODO: retrieve value to revert to directly from store
+    // TODO: don't modify cursor position?
+    editor.setValue(listing.config || '', 1);
+    return showApplicableOps();
+  }
+
+  listingEntry.getPart('config-revert')
+    .addEventListener('click', revertConfig);
+
+
   var statusOutput = listingEntry.getPart('pull-status');
   var pullButton = listingEntry.getPart('pull-button');
 
   function performPull() {
     var port = chrome.runtime.connect();
     port.postMessage({type: 'startPull', listingId: listing.id});
-    pullButton.disabled = true;
+    pullInProgress = true;
+    showApplicableOps();
 
     function updateStatus(message) {
       if (message.type == 'status') {
-        statusOutput.hidden = false;
         statusOutput.textContent = message.message;
       } else if (message.type == 'finish') {
-        statusOutput.hidden = true;
+        pullInProgress = false;
+        showApplicableOps();
       } else if (message.type == 'error') {
-        statusOutput.hidden = false;
+        // TODO: make it possible to escape this
         statusOutput.textContent = 'Error: ' + message.error.message;
         console.error(message.error);
       }
@@ -81,7 +166,8 @@ function createListingEntry(listing) {
 
     port.onMessage.addListener(updateStatus);
     port.onDisconnect.addListener(function(){
-      pullButton.disabled = false;
+      pullInProgress = false;
+      showApplicableOps();
     });
   }
 
--- partywall-styles.js
+++ partywall-styles.js
@@ -3,4 +3,7 @@ var styler = partywallStyler();
 
 styler.addStyle([['.listing','#config-section']],
   'flex: 3;');
-
+styler.addStyle([['.listing','#dir-location']],
+  'flex: 1;');
+styler.addStyle([['.listing','#dir-changer']],
+  'flex: none;');
